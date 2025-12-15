00ae30706e16d417db4f6cc122b8f20f99ee264f
tianquege <wangn5635@gmail.com>
2025-12-15 17:42:15 +0800

    fix: V2bX.sh config generation json comma error

--- V2bX.sh
+++ V2bX.sh
@@ -600,8 +600,10 @@ generate_config_file() {
 
 
     # 移除最后一个逗号并关闭数组
+    if [[ "$cores_config" == *"," ]]; then
+        cores_config="${cores_config%,}"
+    fi
     cores_config+="]"
-    cores_config=$(echo "$cores_config" | sed 's/},]$/}]/')
 
     # 切换到配置文件目录
     cd /etc/V2bX
